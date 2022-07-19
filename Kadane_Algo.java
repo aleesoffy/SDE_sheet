a6318aeeaea6eb7ee949ad2a9223ebe020ee8c7c
aleesoffy <91193245+aleesoffy@users.noreply.github.com>
2022-07-19 18:18:50 +0530

    Create Kadane_Algo.java

---
+++ Kadane_Algo.java
@@ -0,0 +1,25 @@
+// max subarray with time complexity O(n)
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==1){
+         
+        }
+        
+        int sum=0;
+        int maxSum=nums[0];
+        for(int i=0;i<nums.length;i++){
+           sum +=nums[i];
+            if(sum>maxSum){
+           
+                maxSum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
